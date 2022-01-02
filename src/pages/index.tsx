4e86957b82458cc30ad757eaab9abb1e04b74887
Thiago Andrade Silva <thiago.adsix@gmail.com>
2022-01-02 00:40:11 -0300

    refactor: add ssg to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
 import { SubscribeButton } from '../components/SubscribeButton'
@@ -39,7 +39,7 @@ export default function Home({ product }: HomePros) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KDG4zDgyFyF6yZYiY02nAy9')
 
   const product = {
@@ -51,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   }
 
   return {
-    props: { product }
+    props: { product },
+    revalidate: 60 * 60 * 24 // 24h
   }
 }
