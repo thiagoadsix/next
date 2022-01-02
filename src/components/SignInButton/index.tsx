5b032964780d7dc8d60f29b4a38ddd01842785a4
Thiago Andrade Silva <thiago.adsix@gmail.com>
2022-01-02 05:16:40 -0300

    feart: add next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,31 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 import styles from './styles.module.sass';
 
 export function SignInButton() {
-  const isUserLoggedIn = true
+  const { data } = useSession()
 
-  return isUserLoggedIn ? (
+  return data ? (
     <button
       className={styles.signInButton}
       type="button"
     >
       <FaGithub color='#04d361' />
-      Thiago Andrade
-      <FiX color='#737380' className={styles.closeIcon} />
+      {data.user.name}
+      <FiX
+        color='#737380'
+        className={styles.closeIcon}
+        onClick={() => signOut()} 
+      />
     </button>
   ) : (
     <button
       className={styles.signInButton}
       type="button"
+      onClick={() => signIn('github')}
     >
       <FaGithub color='#eba418' />
       Sign in with GitHub
