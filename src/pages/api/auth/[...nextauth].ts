92f97b2d4965428a1ca652d54a677316b6524fe5
Thiago Andrade Silva <thiago.adsix@gmail.com>
2022-01-03 07:35:00 -0300

    feat: add logic to verify if user exists, faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,13 +20,29 @@ export default NextAuth({
     async signIn({ user, account, profile, email, credentials }) {
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            {
-              data: {
-                email: user.email
-              } 
-            }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {
+                data: {
+                  email: user.email
+                } 
+              }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
 
