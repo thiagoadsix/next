5824474b45a13b6b7097a257d721544a3f1330a2
Thiago Andrade Silva <thiago.adsix@gmail.com>
2022-01-05 21:11:17 -0300

    feat: add logic to deal with CMS (Prismic)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,24 @@
+import { ActiveLink } from '../ActiveLink';
+
 import { SignInButton } from '../SignInButton';
 
 import styles from './styles.module.sass';
 
 export function Header() {
+
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news logo" />
 
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" prefetch activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
